f6eca6324f669d043f04ee7886db85b6ed2881ab
Slaik1 <marinakolmik@gmail.com>
2023-06-08 00:46:55 +0300

    1.Clear code 2.Added logic in script.js 3.Added .ico

--- js/script.js
+++ js/script.js
@@ -1,2 +1,53 @@
-const tacksList = document.querySelectorAll('.tacks-list')
-btnAdd = document.querySelectorAll('.tacks-list')
+document.addEventListener('DOMContentLoaded',() =>{
+    const container = document.querySelector('.container__input').children
+
+    const elements={
+        input:container[0],
+        btnAdd:container[1],
+        tasks:document.querySelector('.container__tasks'),
+        alert:document.querySelector('.container__alert')
+    }
+
+    function addTask(text){
+        const li=document.createElement('li')
+        const span=document.createElement('span')
+        const img=document.createElement('img')
+
+        span.innerHTML = text
+        img.src='./icons/delete.svg'
+
+        li.appendChild(span)
+        li.appendChild(img)
+        elements.tasks.appendChild(li)
+        saveData()
+    }
+
+    function saveData(){
+        localStorage.setItem('data',elements.tasks.innerHTML)
+    }
+    
+    function showData(){
+        elements.tasks.innerHTML=localStorage.getItem('data')
+    }
+
+    elements.btnAdd.onclick=()=>{
+        if(elements.input.value === ''){
+            elements.alert.style.visibility='visible'
+        }else{
+            addTask(elements.input.value)
+            elements.input.value=''
+            elements.alert.style.visibility='hidden'
+        }
+    }
+
+    elements.tasks.addEventListener('click',function(e){
+        if(e.target.tagName === 'SPAN'){
+            e.target.classList.toggle('done')
+            saveData()
+        }else if(e.target.tagName === 'IMG'){
+            e.target.parentNode.remove()
+            saveData()
+        }
+    })
+    showData()
+})
